492593476990d608fd9bea006b9ee7c025ecc11d
deepindo <732872042@qq.com>
2020-02-25 22:24:57 +0800

    create a single view app

---
+++ DDPopup/DDPopup/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DDPopup
+//
+//  Created by deepin do on 2020/2/25.
+//  Copyright © 2020 deepin do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = UIColor.cyan
+    }
+
+
+}
+
